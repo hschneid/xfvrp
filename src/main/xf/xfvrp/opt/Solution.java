7d2a0248bb946817ac6caabc747754ef9af84345
hschneid <hschneid@cyberlion.fritz.box>
2017-02-20 20:21:21 +0100

    Solution object as wrapper around linear Node array

---
+++ src/main/xf/xfvrp/opt/Solution.java
@@ -0,0 +1,23 @@
+package xf.xfvrp.opt;
+
+import xf.xfvrp.base.Node;
+
+public class Solution {
+
+	private Node[] giantRoute;
+
+	/**
+	 * @return the giantRoute
+	 */
+	public Node[] getGiantRoute() {
+		return giantRoute;
+	}
+
+	/**
+	 * @param giantRoute the giantRoute to set
+	 */
+	public void setGiantRoute(Node[] giantRoute) {
+		this.giantRoute = giantRoute;
+	}
+	
+}
