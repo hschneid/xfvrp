da270cc41802108071ced2ffee83e94276d08e60
hschneid <hschneid@cyberlion.fritz.box>
2017-02-20 20:17:16 +0100

    Solution object added

---
+++ src/main/xf/xf2Evrp/Solution.java
@@ -0,0 +1,12 @@
+package xf.xf2Evrp;
+
+import java.util.Map;
+
+public class Solution {
+
+	public String shortHaulPreset;
+	public String longHaulPreset;
+	
+	public Map<String, Float> pickupTimes;
+	public Map<String, Integer> satelliteAllocation;
+}
