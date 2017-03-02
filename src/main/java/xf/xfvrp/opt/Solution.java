616cf62f9895ee0e2bc29e4a82ead454beab5c91
hschneid <hschneid@cyberlion.fritz.box>
2017-03-02 22:50:17 +0100

    Change in Solution from linear to 2dim array

--- src/main/java/xf/xfvrp/opt/Solution.java
+++ src/main/java/xf/xfvrp/opt/Solution.java
@@ -1,30 +1,73 @@
 package xf.xfvrp.opt;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import xf.xfvrp.base.Node;
+import xf.xfvrp.base.SiteType;
 
 public class Solution {
 
-	private Node[] giantRoute;
+	private Node[][] routes;
 
 	/**
 	 * @return the giantRoute
 	 */
 	public Node[] getGiantRoute() {
-		return giantRoute;
+		List<Node> giantRoute = new ArrayList<>();
+		for (Node[] route : routes) {
+			for (int i = 0; i < route.length - 1; i++) {
+				giantRoute.add(route[i]);
+			}
+		}
+		
+		Node[] lastRoute = routes[routes.length - 1];
+		giantRoute.add(lastRoute[lastRoute.length - 1]);
+		
+		return giantRoute.toArray(new Node[0]);
 	}
 
 	/**
 	 * @param giantRoute the giantRoute to set
 	 */
 	public void setGiantRoute(Node[] giantRoute) {
-		this.giantRoute = giantRoute;
+		if(giantRoute.length == 0)
+			routes = new Node[0][0];
+		
+		List<List<Node>> list = new ArrayList<>();
+		List<Node> currentRoute = new ArrayList<>();
+		currentRoute.add(giantRoute[0]);
+		Node lastDepot = giantRoute[0];
+		for (int i = 1; i < giantRoute.length; i++) {
+			Node currentNode = giantRoute[i];
+			if(currentNode.getSiteType() == SiteType.DEPOT) {
+				currentRoute.add(lastDepot);
+				list.add(currentRoute);
+				currentRoute = new ArrayList<>();
+				currentRoute.add(currentNode);
+				
+				lastDepot = currentNode;
+			} else {
+				currentRoute.add(currentNode);
+			}
+		}
+		
+		routes = new Node[list.size()][];
+		for (int i = 0; i < list.size(); i++)
+			routes[i] = list.get(i).toArray(new Node[0]);
 	}
-	
+
+	/**
+	 * 
+	 * @return
+	 */
 	public Solution copy() {
 		Solution solution = new Solution();
-		solution.setGiantRoute(Arrays.copyOf(giantRoute, giantRoute.length));
+		Node[][] copyRoutes = new Node[routes.length][];
+		for (int i = 0; i < routes.length; i++)
+			copyRoutes[i] = Arrays.copyOf(routes[i], routes[i].length);		
+		solution.routes = copyRoutes;
 		
 		return solution;
 	}
