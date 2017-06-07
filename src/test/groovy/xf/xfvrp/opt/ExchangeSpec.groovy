fd6950861189fefeb032e9266ca8de5f2064f726
hschneid <hschneid@cyberlion.fritz.box>
2017-06-07 21:01:16 +0200

    Specs for move and exchange

---
+++ src/test/groovy/xf/xfvrp/opt/ExchangeSpec.groovy
@@ -0,0 +1,120 @@
+package xf.xfvrp.opt
+
+import spock.lang.Specification
+import xf.xfvrp.base.Node
+import xf.xfvrp.base.SiteType
+import xf.xfvrp.opt.improve.XFVRPRelocate
+
+class ExchangeSpec extends Specification {
+
+	def service = new XFVRPRelocate();
+	
+	def "Regular exchange - same position"() {
+		def n1 = new Node(externID: "1", siteType: SiteType.DEPOT);
+		def n2 = new Node(externID: "2", siteType: SiteType.CUSTOMER);
+		def n3 = new Node(externID: "3", siteType: SiteType.CUSTOMER);
+		def n4 = new Node(externID: "4", siteType: SiteType.DEPOT);
+		
+		def sol = new Solution()
+		sol.setGiantRoute([n1, n2, n3, n4] as Node[])
+		
+		when:
+		service.exchange(sol, 1, 1);
+		
+		def result = sol.getGiantRoute()
+		
+		then:
+		result[0].externID == "1"
+		result[1].externID == "2"
+		result[2].externID == "3"
+		result[3].externID == "1"
+	}
+	
+	def "Regular exchange - one route"() {
+		def n1 = new Node(externID: "1", siteType: SiteType.DEPOT);
+		def n2 = new Node(externID: "2", siteType: SiteType.CUSTOMER);
+		def n3 = new Node(externID: "3", siteType: SiteType.CUSTOMER);
+		def n4 = new Node(externID: "4", siteType: SiteType.DEPOT);
+		
+		def sol = new Solution()
+		sol.setGiantRoute([n1, n2, n3, n4] as Node[])
+		
+		when:
+		service.exchange(sol, 1, 2);
+		
+		def result = sol.getGiantRoute()
+		
+		then:
+		result[0].externID == "1"
+		result[1].externID == "3"
+		result[2].externID == "2"
+		result[3].externID == "1"
+	}
+	
+	def "Regular exchange - two routes"() {
+		def n1 = new Node(externID: "1", siteType: SiteType.DEPOT);
+		def n2 = new Node(externID: "2", siteType: SiteType.CUSTOMER);
+		def n3 = new Node(externID: "3", siteType: SiteType.CUSTOMER);
+		def n4 = new Node(externID: "4", siteType: SiteType.DEPOT);
+		def n5 = new Node(externID: "5", siteType: SiteType.CUSTOMER);
+		def n6 = new Node(externID: "6", siteType: SiteType.CUSTOMER);
+		def n7 = new Node(externID: "7", siteType: SiteType.DEPOT);
+
+		def sol = new Solution()
+		sol.setGiantRoute([n1, n2, n3, n4, n5, n6, n7] as Node[])
+		
+		when:
+		service.exchange(sol, 2, 4);
+		
+		def result = sol.getGiantRoute()
+		
+		then:
+		result[0].externID == "1"
+		result[1].externID == "2"
+		result[2].externID == "5"
+		result[3].externID == "4"
+		result[4].externID == "3"
+		result[5].externID == "6"
+		result[6].externID == "4"
+	}
+	
+	def "Exchange - at the border"() {
+		def n1 = new Node(externID: "1", siteType: SiteType.DEPOT);
+		def n2 = new Node(externID: "2", siteType: SiteType.CUSTOMER);
+		def n3 = new Node(externID: "3", siteType: SiteType.CUSTOMER);
+		def n4 = new Node(externID: "4", siteType: SiteType.DEPOT);
+		
+		def sol = new Solution()
+		sol.setGiantRoute([n1, n2, n3, n4] as Node[])
+		
+		when:
+		service.exchange(sol, 1, 0);
+		
+		def result = sol.getGiantRoute()
+		
+		then:
+		result[0].externID == "2"
+		result[1].externID == "1"
+		result[2].externID == "3"
+		result[3].externID == "1"
+	}
+
+	def "Exchange - with exception"() {
+		def n1 = new Node(externID: "1", siteType: SiteType.DEPOT);
+		def n2 = new Node(externID: "2", siteType: SiteType.CUSTOMER);
+		def n3 = new Node(externID: "3", siteType: SiteType.CUSTOMER);
+		def n4 = new Node(externID: "4", siteType: SiteType.DEPOT);
+		
+		def sol = new Solution()
+		sol.setGiantRoute([n1, n2, n3, n4] as Node[])
+		
+		when:
+		service.exchange(sol, 2, 4);
+		
+		def result = sol.getGiantRoute()
+		
+		then:
+		thrown ArrayIndexOutOfBoundsException
+	}
+
+}
